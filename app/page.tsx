d83d1364a1659f048381dc7a3c6de92a7f0a975e
Luis <luis.luis@icloud.com>
2025-01-16 14:43:01 -0600

    update footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
 import Form from "@/components/ui/Form";
-import { Calculator, Github } from "lucide-react";
+import { Calculator } from "lucide-react";
+import Footer from "@/components/ui/Footer";
 
 export default function Home() {
   return (
@@ -16,46 +16,7 @@ export default function Home() {
         </div>
         <Form />
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://www.canada.ca/en/financial-consumer-agency/services/financial-toolkit/taxes/taxes-2/5.html"
-          target="_blank"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://investinganswers.com/dictionary/m/marginal-tax-rate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://github.com/CLu1s/points"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Github height={16} width={16} />
-          Go to repo →
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
 }
